81733f9f9859faf8d01c4fd0da9d52f1ee5fc007
sh19870501 <sh11821783@icloud.com>
2024-05-06 22:52:07 +0900

    App.vue内の学習その3

--- Documents/my_vue_learning_app/vue-lesson/src/App.vue
+++ Documents/my_vue_learning_app/vue-lesson/src/App.vue
@@ -1,96 +1,105 @@
-<!-- .vueファイルは単一コンポーネントと呼ばれ、以下のようなHTMLタグ（JavaScript、HTML、CSS）で構成されています。 -->
-
-<!-- <script setup>タグ: コンポーネントのセットアップを定義し、HTMLの属性のようなものを記述する必要があります。 -->
-
-<!-- template以外なくても表示される -->
-
-<!-- <script setup>で定義されたデータが更新された場合、画面も自動的に更新されます。 -->
+<!-- ref 関数を import して textInput 変数を定義します。ref 関数の引数には初期値を設定することができます。初期値にはブランクが設定されています。-->
 <script setup>
-// Vue.jsからrefをインポート
-import { ref, reactive } from 'vue'
+import { ref } from 'vue'
 
-// 変数titleを定義し、初期値を'Vue.js Course'とする
-const title = ref('Vue.js Course')
+// 入力欄に初期値として表示するための textInput 変数を ref 関数を使って定義します。
+const textInput = ref('初期値はここに入れておく')
 
-// 変数priceをrefでラップし、初期値を9.99とする
-// ラップ：オブジェクトを参照型に変換することで、Vueがリアクティブな変更を検出できるようにする
-// リアクティブ：変更があった際にそれを自動的に検知し、関連するビューを更新する仕組み
-let price = ref(9.99)
+// チェックボックスのプロパティは真偽値を取るので、初期値を true とします。
+const checkValue = ref(true)
 
-// increment関数の定義：priceの値を1増やす
-function increment() {
-  price.value += 1
-  // instructorのageをインクリメントする
-  // インクリメントとは、数値を1つ増やす操作を指します。つまり、何かの値を「1つ増やす」という意味です。例えば、変数の値を1ずつ増やす場合、その変数をインクリメントすると言います。
-  instructor.age += 1
-  // instructorのsnsオブジェクト内のtwitterプロパティを変更する
-  // 「プロパティ」という言葉は、オブジェクト指向プログラミングにおいて、オブジェクトに属するデータや機能を指します。
-  // オブジェクトは、データ（プロパティ）とそのデータに関連する操作（メソッド）をまとめたもの
-  // 例えば、車のオブジェクトがある場合、その車の色やサイズ、重量などがプロパティとなります。
-  // プロパティは名前（キー）と値のペアで構成され、名前を使って値にアクセスしたり、値を変更したりすることができます。
-  instructor.sns.twitter = 'hello'
-  instructor.email = 'taro@test.com'
+// チェックがされている場合は "はい"、されていない場合は "いいえ" が表示されます。
+const checkValue1 = ref('はい')
+
+// ラグビー、サッカー、バスケットボールのチェックボックスの初期値は空の配列です。
+const checkValue2 = ref([])
+
+// 初期値として "野球" を持つ配列を定義します。この値はチェックボックスの選択に影響しません。
+const checkValue2A = ref(['野球'])
+
+// ラジオボタンの初期値は空です。
+const radioValue3 = ref('')
+
+// セレクトボックスの初期値は空です。
+const selectValue = ref('')
+
+// マルチセレクトボックスの初期値は空の配列です。
+const selectValues = ref([])
+
+// ファイルのデータを保持するための変数であり、ファイルの内容を監視し、変更があった場合に Vue コンポーネントの再レンダリングをトリガーします。
+const fileData = ref('')
+
+// ファイルのデータを処理するための handleFile メソッドを定義します。
+const handleFile = (event) => {
+  // event オブジェクトからファイルを取得します。
+  const file = event.target.files[0]
+  // fileData の値を選択されたファイルに設定します。
+  fileData.value = file
 }
-
-// infoというオブジェクトをrefでラップし、初期値をstudents: 1000, rating: 4とする
-const info = ref({
-  // 学生の数
-  students: 1000,
-  // 評価
-  rating: 4
-})
-// instructorオブジェクトをreactiveでラップし、リアクティブなオブジェクトにする
-const instructor = reactive({
-  // インストラクターの名前
-  name: 'sunao',
-  // インストラクターの年齢
-  age: 37,
-  // インストラクターのSNS情報
-  sns: {
-    // Twitterアカウント
-    twitter: '@_test_',
-    // YouTubeアカウント
-    youtube: '@test'
-  },
-  // instructorオブジェクトのemailプロパティをリアクティブな参照型で定義し、初期値を'test_sunao@test.com'とする
-  email: ref('test_sunao@test.com')
-})
-// courseInfoオブジェクトを定義し、リアクティブな参照型でsectionsとlanguageプロパティを持たせる
-const corseInfo = {
-  // コースのセクション数
-  sections: ref(10),
-  // コースの言語
-  lauguage: ref('Japanese')
-}
-// コースのセクション数をコンソールに出力する
-console.log(corseInfo.sections.value)
-// infoのstudentsプロパティの値をコンソールに出力する
-console.log(info.value.students)
-// instructorオブジェクトのnameをコンソールに出力する
-console.log(instructor.name)
-// instructorオブジェクトのageをコンソールに出力する
-console.log(instructor.age)
-// instructorオブジェクトのemailプロパティをコンソールに出力する
-console.log(instructor.email)
 </script>
 
+<!-- Vue.js のディレクティブを使って双方向バインディングを実現します。 -->
 <template>
-  <!-- 変数titleを表示する -->
-  <h1>Title: {{ title }}</h1>
-  <!-- priceの値を表示し、1を引いた値を表示する -->
-  <h2>Price: ${{ price - 1 }}</h2>
-  <!-- ボタンがクリックされたときにincrement関数を実行する -->
-  <button @click="increment">button</button>
-  <!-- infoのstudentsプロパティの値を表示する -->
-  <h2>Students: {{ info.students }}</h2>
-  <!-- instructorのnameを表示する -->
-  <h2>Instructor: {{ instructor.name }}</h2>
-  <!-- instructorのageを表示する -->
-  <h2>Instructor-age: {{ instructor.age }}</h2>
-  <!-- instructorのsnsオブジェクト内のtwitterプロパティを表示する -->
-  <h2>Instructor-sns-twitter: {{ instructor.sns.twitter }}</h2>
-  <!-- instructorのemailプロパティを表示する -->
-  <h2>Instructor-email: {{ instructor.email }}</h2>
-  <!-- courseInfoオブジェクトのsectionsプロパティの値を表示する -->
-  <h2>Course-info-Sections: {{ corseInfo.sections.value }}</h2>
+  <div id="app">
+    <!-- テキストボックスに入力した内容が textInput の値に反映されます。 -->
+    <!-- @input イベントを使ってテキストボックスの入力内容が変更された際に textInput の値を更新します。 -->
+    <input :value="textInput" @input="textInput = $event.target.value" />
+    <!-- textInput の値を表示します。 -->
+    <p>{{ textInput }}</p>
+    <!-- textInput1 の値を表示します。 -->
+    <p v-text="textInput1"></p>
+
+    <!-- checkValue の値を表示します。 -->
+    <input v-model="checkValue" type="checkbox" />
+    <p>{{ checkValue }}</p>
+
+    <!-- checkValue1 の値を表示します。 -->
+    <input v-model="checkValue1" type="checkbox" true-value="はい" false-value="いいえ" />
+    <p>{{ checkValue1 }}</p>
+
+    <!-- checkValue2 の値を表示します。 -->
+    <input v-model="checkValue2" type="checkbox" value="ラグビー" /> ラグビー
+    <input v-model="checkValue2" type="checkbox" value="サッカー" /> サッカー
+    <input v-model="checkValue2" type="checkbox" value="バスケットボール" /> バスケットボール
+    <p>{{ checkValue2 }}</p>
+
+    <!-- checkValue2A の値を表示します。 -->
+    <input v-model="checkValue2A" type="checkbox" value="ラグビー" /> ラグビー
+    <input v-model="checkValue2A" type="checkbox" value="サッカー" /> サッカー
+    <input v-model="checkValue2A" type="checkbox" value="バスケットボール" /> バスケットボール
+    <p>{{ checkValue2A }}</p>
+
+    <!-- radioValue3 の値を表示します。 -->
+    <input v-model="radioValue3" type="radio" value="ラグビー" /> ラグビー
+    <input v-model="radioValue3" type="radio" value="サッカー" /> サッカー
+    <input v-model="radioValue3" type="radio" value="バスケットボール" /> バスケットボール
+    <p>{{ radioValue3 }}</p>
+
+    <!-- selectValue の値を表示します。 -->
+    <select v-model="selectValue">
+      <!-- 興味のあるスポーツを選択するセレクトボックスです。 -->
+      <option disabled value="">興味のあるスポーツを選択</option>
+      <option>ラグビー</option>
+      <option>サッカー</option>
+      <option>バスケットボール</option>
+    </select>
+    <p>{{ selectValue }}</p>
+
+    <!-- selectValues の値を表示します。 -->
+    <select v-model="selectValues" multiple="">
+      <!-- マルチセレクトボックスの選択肢です。 -->
+      <option>ラグビー</option>
+      <option>サッカー</option>
+      <option>バスケットボール</option>
+    </select>
+    <p>{{ selectValues }}</p>
+
+    <!-- ファイルをアップロードするための input 要素です。 -->
+    <!-- ファイルが選択されると handleFile メソッドが実行され、選択されたファイルの情報が fileData にセットされます。 -->
+    <div id="app">
+      <input type="file" @change="handleFile" />
+      <!-- 選択されたファイルの情報を表示します。 -->
+      {{ fileData }}
+    </div>
+  </div>
 </template>
